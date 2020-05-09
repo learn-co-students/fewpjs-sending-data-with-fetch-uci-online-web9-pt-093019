aaaa0b33a6dd50e2d4d370f7d666ebac07491211
Alice Freda <alicefreda@gmail.com>
2020-05-08 17:20:21 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    let userData = {
+        name: userName,
+        email: userEmail
+    }
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+      };
+
+      return fetch("http://localhost:3000/users", configObj)
+      .then(function(response) {
+          return response.json();  
+      })
+      .then(function(object) {
+        console.log(object);
+    })
+    .catch(function(error) {
+        alert("Error! Couldn't fetch object");
+        console.log(error.message);
+      });
+}
